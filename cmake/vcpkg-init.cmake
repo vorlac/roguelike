c653e1a40b37e3b7f5933543a8317c25aec5abdc
sal <sal@orlac.io>
2023-10-09 01:02:31 -0400

    added vcpkg init back in for automated builds

--- cmake/vcpkg-init.cmake
+++ cmake/vcpkg-init.cmake
@@ -1,3 +1,13 @@
+# entityx submodule init/update
+if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/extern/entityx/entityx")
+    message(NOTICE "entityx submodule sources not found")
+    message(NOTICE "initializing/updating the entityx submodule...")
+    execute_process(
+        COMMAND git submodule update --init extern/entityx
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
 
 # VCPKG submodule init/update
 if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/extern/vcpkg/ports")
@@ -17,5 +27,40 @@ if(NOT CMAKE_TOOLCHAIN_FILE)
         set(CMAKE_TOOLCHAIN_FILE "${toolchain_file_path}")
     else()
         message(WARNING "VCPKG toolchain file not found: ${toolchain_file_path}")
+    endif()
+endif()
+
+# =======================================================================
+# VCPKG bootstrap / initialization.
+# =======================================================================
+if(WIN32)
+    set(vcpkg_executable "${CMAKE_CURRENT_SOURCE_DIR}/extern/vcpkg/vcpkg.exe")
+else()
+    set(vcpkg_executable "${CMAKE_CURRENT_SOURCE_DIR}/extern/vcpkg/vcpkg")
+endif()
+
+if(EXISTS "${vcpkg_executable}")
+    message(NOTICE "Found VCPKG Executable: ${vcpkg_executable}")
+else()
+    message(NOTICE "Could not find VCPKG Executable: ${vcpkg_executable}")
+    message(NOTICE "Calling VCPKG bootstrap scripts.")
+
+    if(WIN32)
+        execute_process(
+            COMMAND powershell -c "${CMAKE_CURRENT_SOURCE_DIR}/extern/vcpkg/bootstrap-vcpkg.bat"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    elseif(UNIX)
+        execute_process(
+            COMMAND bash "${CMAKE_CURRENT_SOURCE_DIR}/extern/vcpkg/bootstrap-vcpkg.sh"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    endif()
+
+    # fail out if vcpkg isn't found after setup
+    if(NOT EXISTS "${vcpkg_executable}")
+        message(FATAL_ERROR "ERROR: '${vcpkg_executable}' not found!")
     endif()
 endif()
