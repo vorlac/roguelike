ec23829f03c424826dabac2984d79d4b4b2f5145
sal <sal@orlac.io>
2024-01-04 18:57:02 -0500

    ported toolbutton

---
+++ src/core/ui/gui.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "core/ui/button.hpp"
+#include "core/ui/label.hpp"
+#include "core/ui/layout.hpp"
+#include "core/ui/popup.hpp"
+#include "core/ui/popupbutton.hpp"
+#include "core/ui/theme.hpp"
+#include "core/ui/toolbutton.hpp"
+#include "core/ui/ui_icons.hpp"
+#include "core/ui/vscrollpanel.hpp"
+#include "core/ui/widget.hpp"
---
+++ src/core/ui/toolbutton.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+#include "core/ui/button.hpp"
+
+namespace rl::ui {
+
+    /// @brief
+    ///     Simple radio+toggle button with an icon.
+    class ToolButton : public ui::Button
+    {
+    public:
+        ToolButton(ui::widget* parent, i32 icon, const std::string& caption = "")
+            : ui::Button(parent, caption, icon)
+        {
+            this->set_flags(Button::Flags(Button::Flags::RadioButton | Button::Flags::ToggleButton));
+            this->set_fixed_size(ds::dims<i32>{ 25, 25 });
+        }
+    };
+}
