5d45f6faceab65fa12cb576e26da6e2884fa5d19
sal <sal@orlac.io>
2024-04-27 09:48:02 -0400

    added a blacklist for clang-format (stb & icon headers)

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -10,20 +10,33 @@ if (CLANG_FORMAT_PROGRAM)
 
     message("Using clang-format: ${CLANG_FORMAT_PROGRAM} (${CLANG_FORMAT_VERSION})")
 
-    file(GLOB_RECURSE 
-      format_src_list 
-      RELATIVE 
+    file(GLOB_RECURSE
+      format_src_list
+      RELATIVE
         "${CMAKE_CURRENT_SOURCE_DIR}"
           "src/*.[hc]"
           "src/*.[hc]pp"
     )
 
+    set(skip_stb_pattern "src/gfx/stb/stb.*")
+    set(skip_ico_pattern "src/ui/text/icons.*")
+
     foreach(_src_file ${format_src_list})
-        message("    formatting => ${_src_file}")
-        execute_process( 
-          COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
-          WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-        )
+        unset(MATCHED_STB)
+        unset(MATCHED_ICO)
+
+        string(REGEX MATCH ${skip_stb_pattern} MATCHED_STB ${_src_file})
+        string(REGEX MATCH ${skip_ico_pattern} MATCHED_ICO ${_src_file})
+
+        if (MATCHED_STB OR MATCHED_ICO)
+            message("      skipping => ${_src_file}")
+        else()
+            message("    formatting => ${_src_file}")
+                execute_process(
+                COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            )
+        endif()
     endforeach()
 
     unset(CLANG_FORMAT_VERSION)
